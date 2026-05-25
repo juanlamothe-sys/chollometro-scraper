da78293b5f3005196bc53f8f29e1e28c20b82ee6
juanlamothe-sys <juan.lamothe@lge.com>
2026-05-25 10:51:15 +0200

    update2 app.py

--- app.py
+++ app.py
@@ -24,6 +24,7 @@ MERCHANTS = {
     "Amazon (11)": 11,
     "PcComponentes (389)": 389,
     "El Corte Inglés (456)": 456,
+    "LG (1857)": 1857,
     "Otro (manual)": 0,
 }
 
@@ -340,4 +341,5 @@ elif 'df' not in st.session_state:
     | Amazon | 11 |
     | PcComponentes | 389 |
     | El Corte Inglés | 456 |
+    | LG | 1857 |
     """)
